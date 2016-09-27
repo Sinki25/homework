3779dfae6f1bc6333a40f0c0a4dc012a30505a4d
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:38:14 +0500

    Add files via upload

---
+++ Console9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k,N,a,b,f,n,max;
+
+            max = 0;
+            n = 0;
+            N= int.Parse(Console.ReadLine());
+            k= int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a == k)
+                {
+                        n++;
+                    }
+                    else
+                    {
+                        n = 0;
+                    }
+                
+                    if (max < n)
+                        max = n;
+                
+            }
+            Console.WriteLine("Максимальная цепочка {0}", max);
+
+        }
+    }
+}
