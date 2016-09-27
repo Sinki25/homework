de418ef188eec4c0853f126fedb04b4fa7d88e9b
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:26:15 +0500

    Add files via upload

---
+++ ConsoleBit4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBit4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,a;
+
+            N = int.Parse(Console.ReadLine());
+
+            while (N > 0)
+            {
+                a = N % 2;
+                N = N / 2;
+                Console.Write(a);
+            }
+            }
+        }
+    }
+
