8e206dec1a159b5123c56fa9a60bae6d9c62286d
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:42:25 +0500

    Add files via upload

---
+++ ConsoleCifra8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCifra8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2;
+            char y1, y2;
+
+            x1 = int.Parse(Console.ReadLine());
+            y1=char.Parse(Console.ReadLine());
+            x2 = int.Parse(Console.ReadLine());
+            y2 = char.Parse(Console.ReadLine());
+
+            int k = Convert.ToInt32(y2 - y1);
+           
+            if ( (Math.Abs(x1-x2)==2) && (Math.Abs(k)==1))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+               
+            }
+        }
+    }
+}
