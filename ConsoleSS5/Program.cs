0278ed72367a5a72d1efb7799dcf62f8cb6e7d7f
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:28:47 +0500

    Add files via upload

---
+++ ConsoleSS5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSS5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,M,k,m,a;
+            String A;
+
+
+            Console.WriteLine("Введите два числа");
+            N = int.Parse(Console.ReadLine());
+            M = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите системы счисления");
+            k = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+
+            A = "";
+            while (N>0)
+            {
+                a = Convert.ToChar(N % k);
+                A+=a;
+                N = N / k;
+            }
+
+            char[] array = A.ToCharArray();
+            Array.Reverse(array);
+            A = new string(array);
+
+            Console.WriteLine("Первое число {0}",A);
+            A = "";
+            while (M > 0)
+            {
+                a = Convert.ToChar(M % m);
+                A += a;
+                M = M / m;
+            }
+
+            char[] array1 = A.ToCharArray();
+            Array.Reverse(array1);
+            A = new string(array1);
+            Console.WriteLine("Второе число {0}", A);
+
+           
+
+        }
+    }
+}
