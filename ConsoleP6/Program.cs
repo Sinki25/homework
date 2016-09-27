571609dc43714a3e6516768992b2b3c0ebf4088a
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:30:44 +0500

    Add files via upload

---
+++ ConsoleP6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,k,a,P;
+
+            N = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+
+            P = 1;
+
+            while (N>0)
+            {
+                a = N % k;
+                N = N / k;
+                P = P * a;
+            }
+
+            Console.WriteLine("Произведение равно {0}", P);
+
+        }
+    }
+}
