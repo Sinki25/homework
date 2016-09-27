6b978ed7330ef984af39b7e0a43d2bf1278b6ffd
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:19:14 +0500

    Add files via upload

---
+++ HW1/ConsoleBilet3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBilet3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,a,b,c,d,e,f;
+
+            N = int.Parse(Console.ReadLine());
+
+            f = N % 10;
+            N = N / 10;
+            e = N % 10;
+            N = N / 10;
+            d = N % 10;
+            N = N / 10;
+            c = N % 10;
+            N = N / 10;
+            b = N % 10;
+            N = N / 10;
+            a = N % 10;
+
+            if (a+c+e==b+d+f)
+            {
+                Console.WriteLine("Билет счастливый");
+            }
+            else
+            {
+                Console.WriteLine("Билет не счастливый");
+            }
+        }
+
+    }
+}
