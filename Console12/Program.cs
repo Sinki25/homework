4d685222bc73b8a0ad94c03f512a262b365dbc71
Sinki25 <ms.sinkovets@mail.ru>
2016-09-27 14:40:55 +0500

    Add files via upload

---
+++ Console12/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, s1, s2,k;
+
+            n = int.Parse(Console.ReadLine());
+            k = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                s1 = 0;
+                for (int i1 = 1; i1 <= i; i1++)
+                {
+                    if (i % i1 == 0)
+                        s1 = s1 + i1;
+                }
+
+                for (int j = 1; j <= n; j++)
+                {
+                    s2 = 0;
+                    for (int j1 = 1; j1 <= j; j1++)
+                    {
+                        if (j % j1 == 0)
+                            s2 = s2 + j1;
+                    }
+                    if ((s1 == s2) && (s1 == i + j) && (i + j < n))
+                    {
+                        Console.Write(i);
+                        Console.Write(" ");
+                        Console.Write(j);
+                        Console.WriteLine();
+                        k = 1;
+                    }
+                    
+                }
+             }
+            if (k==0)
+                    Console.WriteLine("Нет дружественных чисел");
+            
+        }
+    }
+}
+
---
+++ Console12/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Console12")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Console12")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9395cb99-a3c8-4c12-96f2-1618e3c2de7c")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
